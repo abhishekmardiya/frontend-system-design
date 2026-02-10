07916b34585b00f88c310ecd36f1c8d9f62876a8
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-02-10 21:08:46 +0530

    feat: add Biome configuration and package management for frontend system design

--- 01-networking/01_rest-api/index.js
+++ 01-networking/01_rest-api/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { todos } from "./data.js"
+import bodyParser from "body-parser";
+import express from "express";
+import { todos } from "./data.js";
 
 const app = express();
 
@@ -8,65 +8,62 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/todos", (req, res) => {
-    res.status(200).json({
-        message: "todo fetched successfully",
-        todos: todos
-    })
-})
-
+	res.status(200).json({
+		message: "todo fetched successfully",
+		todos: todos,
+	});
+});
 
 app.post("/todos", (req, res) => {
-    const newTodo = req.body;
-    todos.push(newTodo);
+	const newTodo = req.body;
+	todos.push(newTodo);
 
-    res.status(201).json({
-        message: "new todo added successfully",
-        todos: todos
-    })
-})
+	res.status(201).json({
+		message: "new todo added successfully",
+		todos: todos,
+	});
+});
 
 app.put("/todos/:id", (req, res) => {
-    const { id } = req.params;
-    const newTodo = req.body;
+	const { id } = req.params;
+	const newTodo = req.body;
 
-    const todoIndex = todos?.findIndex((todo) => todo?.id === id)
+	const todoIndex = todos?.findIndex((todo) => todo?.id === id);
 
-    if (todoIndex !== -1) {
-        todos[todoIndex] = {
-            id,
-            ...newTodo
-        }
+	if (todoIndex !== -1) {
+		todos[todoIndex] = {
+			id,
+			...newTodo,
+		};
 
-        res.json({
-            message: "todo updated successfully",
-            todos: todos
-        })
-    } else {
-        res.status(400).json({
-            message: "todo id not exists",
-            todos: todos
-        })
-    }
-
-
-})
+		res.json({
+			message: "todo updated successfully",
+			todos: todos,
+		});
+	} else {
+		res.status(400).json({
+			message: "todo id not exists",
+			todos: todos,
+		});
+	}
+});
 
 app.delete("/todos/:id", (req, res) => {
-    const { id } = req.params;
-    const newTodo = req.body;
+	const { id } = req.params;
+	const newTodo = req.body;
 
-    const todoIndex = todos?.findIndex((todo) => todo?.id === id)
+	const todoIndex = todos?.findIndex((todo) => todo?.id === id);
 
-    if (todoIndex !== -1) {
-        todos.splice(todoIndex, 1);
-    }
+	if (todoIndex !== -1) {
+		todos.splice(todoIndex, 1);
+	}
 
-    res.json({
-        message: "todo deleted successfully",
-        todos: todos
-    })
-})
+	res.json({
+		message: "todo deleted successfully",
+		todos: todos,
+	});
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+	console.log("Server is running on http://localhost:3000");
 });
--- 01-networking/02_graphql/server/index.js
+++ 01-networking/02_graphql/server/index.js
@@ -1,17 +1,17 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from "./typeDefs.js"
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./typeDefs.js";
 
 // The ApolloServer constructor requires two parameters: your schema definition and your set of resolvers.
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+	typeDefs,
+	resolvers,
 });
 
 // this will start the apollo server playground at http://localhost:4000
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+	listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
