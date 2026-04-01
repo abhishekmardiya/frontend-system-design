c0bf35173585ade9f097c10ec8057437655da5da
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-04-01 22:21:22 +0530

    refactor: simplify markdownlint configuration and adjust WebSocket client UI layout

--- src/02-communication/03_websocket/index.js
+++ src/02-communication/03_websocket/index.js
@@ -14,14 +14,17 @@ app.get("/", (_req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+// Fires once per browser tab (or client) when the WebSocket handshake completes.
 io.on("connection", (socket) => {
   console.log("Connection established");
 
+  // Client sent a "chat message" event; rebroadcast to every connected socket (simple chat room).
   socket.on("chat message", (msg) => {
     console.log("received message", msg);
     io.emit("chat message", msg);
   });
 
+  // Socket.IO cleans up the socket; use this for logging or per-user teardown.
   socket.on("disconnect", () => {
     console.log("User disconnected!");
   });
