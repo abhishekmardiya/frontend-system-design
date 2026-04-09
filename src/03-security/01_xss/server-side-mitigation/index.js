513a0fd6d9c23989cf150ae3365f392a078736e7
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-04-09 23:14:50 +0530

    refactor: enhance content security policy with detailed nonce rules, update static file serving, and improve HTML structure for CSP demonstration

--- src/03-security/01_xss/server-side-mitigation/index.js
+++ src/03-security/01_xss/server-side-mitigation/index.js
@@ -1,31 +1,56 @@
-import { join } from "node:path";
+import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import express from "express";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const indexHtmlPath = join(__dirname, "public", "index.html");
 
 const PORT = 3010;
 const app = express();
 
-// Policy matches index.html: same-origin + this request’s nonce for trusted inline script; optional
-// third-party script URL; img-src for the external profile image. Inline script without a nonce is blocked.
+/* Rules for Using a CSP Nonce
+- The nonce must be `unique` for every HTTP response
+- It should be generated using a `cryptographically secure` random generator
+- Ensure sufficient length — `at least 128 bits of entropy`
+(≈ 32 hex characters or ~24 base64 characters)
+- <script> tags using a nonce must `not include any untrusted or unescaped data`
+- The nonce value should `only contain valid base64` characters
+*/
+
+// Set the CSP header
 app.use((_req, res, next) => {
   res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self';" +
-      "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;",
+    "Content-Security-Policy", // CSP header
+    // default-src 'self'
+    // → By default, only allow resources from the same origin
+
+    // script-src
+    // → Allow scripts from:
+    //    - same origin ('self')
+    //    - http://unsecure.com
+    //    - inline scripts ONLY if they have the matching nonce
+    // Note:
+    //    - 'unsafe-inline' is ignored when a nonce or hash is present
+
+    // img-src
+    // → Allow images from:
+    //    - same origin
+    //    - https://images.pexels.com
+
+    "default-src 'self'; " +
+      "script-src 'self' 'nonce-randomKey' http://unsecure.com; " +
+      "img-src 'self' https://images.pexels.com/;",
   );
+
   next();
 });
 
-// expose the public folder
-app.use(express.static("public"));
-
+// Serve the HTML page
 app.get("/", (req, res) => {
   console.log(req.url);
-  res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(indexHtmlPath);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://locolhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
